3a10e07785829276c275ecbf390702a576e799b2
AndreiMore <andreiloayza14@gmail.com>
2023-11-25 17:11:28 -0500

    [PM_32] Cuando el jugador entra a la zona se oculta el resto de tilemaps

--- The Last Cat/Assets/EnemyZone.cs
+++ The Last Cat/Assets/EnemyZone.cs
@@ -5,18 +5,33 @@ using Cinemachine;
 
 public class EnemyZone : MonoBehaviour
 {
+    [Header("Contador de enemigos")]
+    [SerializeField] EnemyCounter Contador;
+
+    [SerializeField] CinemachineVirtualCamera camara;
+    [SerializeField] Animator[] animators;
+
     bool PlayerInArea = false;
-    public CinemachineVirtualCamera camara;
 
     private void Update()
     {
         if (PlayerInArea)
         {
             camara.Priority = 11;
+
+            for(int i = 0; i < animators.Length; i++)
+            {
+                animators[i].SetBool("Active", true);
+            }
         }
         else
         {
             camara.Priority = 0;
+
+            for (int i = 0; i < animators.Length; i++)
+            {
+                animators[i].SetBool("Active", false);
+            }
         }
     }
 
