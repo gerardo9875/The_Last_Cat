91499ccd923297ac510dec4ccf3402a100ea0563
AndreiMore <andreiloayza14@gmail.com>
2023-12-02 02:00:37 -0500

    Prefabs del menu de pausa y game over y sprites de la municion de la bomba raton

--- The Last Cat/Assets/Scripts/Player_BombaR.cs
+++ The Last Cat/Assets/Scripts/Player_BombaR.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class Player_BombaR : MonoBehaviour
 {
     public int ammo;
+    [SerializeField] ParticleSystem destroyParticles;
     Player_Disparo shootScript;
     private void Start()
     {
         shootScript = GameObject.Find("Player").GetComponentInChildren<Player_Disparo>();
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") && shootScript.ratonCount < 5)
         {
-            shootScript.ratonCount += 1;
+            shootScript.ratonCount += ammo;
+            Instantiate(destroyParticles, transform.position, destroyParticles.transform.rotation);
             Destroy(gameObject);
         }
     }
